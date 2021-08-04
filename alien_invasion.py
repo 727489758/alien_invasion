3f4b253db38126fbf1c2ff005b7533dd6c0e29f4
727489758 <43691462+727489758@users.noreply.github.com>
2021-08-04 21:47:23 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,19 +19,19 @@ class AlienInvasion:
         while True:
             # 监视键盘和鼠标事件。
             self._check_events()
-            
-            # 每次循环时都重绘屏幕。
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可见。
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """响应按键和鼠标事件"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏。
